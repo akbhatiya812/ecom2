642d22262d2e770dfda3e19f6b6ff6c16ac96d9f
akbhatiya812 <161412219+akbhatiya812@users.noreply.github.com>
2024-02-29 16:19:24 +0530

    Delete src directory

--- src/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const URL = process.env.MONGOOSE_URL;
-
-
-mongoose.connect(URL).then(()=> {
-  // console.log("MongoDb connected successfully");
-}).catch(err => {
-  console.log(err);
-})
--- src/middleware/isAuth.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req,res,next) => {
-    if(!req.session.isLoggedIn || req.session.isLoggedIn === false){
-        return res.redirect('/user/log-in');
-    }
-    next();
-}
